3baaa3c46ea50046707ad947b0d7b856f67740b1
Dillon McQuade <dillonkmcquade@gmail.com>
2023-09-27 19:52:50 -0400

    Set up thymeleaf layout, add tailwind

--- src/main/java/dev/dmcquade/fitnesstracker/FitnessTrackerApplication.java
+++ src/main/java/dev/dmcquade/fitnesstracker/FitnessTrackerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FitnessTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FitnessTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessTrackerApplication.class, args);
+    }
 
 }
